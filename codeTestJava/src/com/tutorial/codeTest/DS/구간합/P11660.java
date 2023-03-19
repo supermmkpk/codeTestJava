a2d76f00580433bd3f0adda1890152bec6586e81
supermmkpk <supermmkpk@gmail.com>
2023-03-20 00:13:30 +0900

    3/19

---
+++ codeTestJava/src/com/tutorial/codeTest/DS/구간합/P11660.java
@@ -0,0 +1,49 @@
+package com.tutorial.codeTest.DS.구간합;
+
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//2차원 구간합
+public class P11660 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken()); //1줄: 배열(표)의 크기 N 입력
+		int query = Integer.parseInt(st.nextToken()); //1줄: 질의 수 query 입력
+		int i, j;
+		
+		//2줄~: 원본 배열 A 입력
+		int A[][] = new int[N+1][N+1];
+		for(i = 1; i <= N; i++) { //행
+			st = new StringTokenizer(br.readLine());	
+			for(j = 1; j <= N; j++)  //열
+				A[i][j] = Integer.parseInt(st.nextToken());
+		}
+		
+		//합 배열 D 생성
+		int D[][] = new int[N+1][N+1];
+		for(i = 1; i <= N; i++)  //행	
+			for(j = 1; j <= N; j++)  //열
+				D[i][j] = D[i-1][j] + D[i][j-1] - D[i-1][j-1] + A[i][j];
+		
+		//다음 query개 줄: (x1,y1) ~ (x2,y2) 입력
+		//구간 합 출력 
+		for (i = 0; i < query; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			
+			int result = D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1];
+			System.out.println(result);
+		}
+		
+		
+
+	}
+
+}
---
+++ codeTestJava/src/com/tutorial/codeTest/DS/투포인터/P2018.java
@@ -0,0 +1,28 @@
+package com.tutorial.codeTest.DS.투포인터;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+//연속된 자연수의 합 구하기
+public class P2018 {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int count = 1;
+		int sum = 1;
+		int start = 1;
+		int end = 1;
+		
+		while(end < N) {
+			if(sum > N)
+				sum -= start++;
+			else if(sum < N)
+				sum += ++end;
+			else {
+				++count;
+				sum += ++end;
+			}
+		}
+		System.out.println(count);
+	}
+}
